3d4bb192854e2f5e568aefd2d637df3fd706d743
wlals2997 <85855544+wlals2997@users.noreply.github.com>
2021-11-09 10:51:35 +0900

    js clcik colors

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const Canvas = document.querySelector('#jsCanvas');
 const context = Canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
 
-Canvas.width = 700;
-Canvas.height = 700;
+Canvas.width = 700; //실제 픽셀사이즈 부여(css, js둘다)
+Canvas.height = 700; //실제 픽셀사이즈 부여(css, js둘다)
 context.strokeStyle = '#2c2c2c';
 context.lineWidth = 2.5; //브러쉬 크기-linewidth
 
@@ -15,22 +16,28 @@ function onMousemove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
-    context.beginPath();
-    context.moveTo(x, y);
+    //경로를 만든다
+    context.beginPath(); //경로생성
+    context.moveTo(x, y); //선 시작 좌표
   } else {
-    context.lineTo(x, y);
+    //그린다
+    context.lineTo(x, y); //선 끝 좌표
 
-    context.stroke();
+    context.stroke(); //선 그리기
   }
 }
-function onMousedown(event) {
-  painting = true; //마우스가 클릭해졌을  때는  true로 변경
-}
+// function onMousedown(event) {
+//   painting = true; //마우스가 클릭해졌을  때는  true로 변경
+// }
 function onMouseup(event) {
   painting = false; //마우스가 클릭했다가 다시 땟을때  false로 변경
 }
 function onMouseleave(event) {
   painting = false; //마우스가 외부로 갔을 때   false로 변경
+}
+function handleColors(event) {
+ const color=event.target.style.backgroundColor;
+ context.strokeStyle = color;
 }
 
 if (Canvas) {
@@ -39,3 +46,6 @@ if (Canvas) {
   Canvas.addEventListener('mouseup', onMouseup);
   Canvas.addEventListener('mouseleave', onMouseleave);
 }
+Array.from(colors).forEach((color) =>
+  color.addEventListener('click', handleColors)
+);
